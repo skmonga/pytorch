c7b41dcdf3c7c8a160a134fa82942213bdb3ce61
asl3 <amandaliu@fb.com>
2022-06-23 12:26:11 -0700

    [quant] Modify APoT nonuniform quantization workflow
    ghstack-source-id: 287e5986bbee2778e6bda149dd046d96b92cea1e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80075

--- torch/ao/quantization/experimental/quantizer.py
+++ torch/ao/quantization/experimental/quantizer.py
@@ -1,43 +1,29 @@
 import torch
 from torch import Tensor
-from torch.ao.quantization.experimental.observer import APoTObserver
+from typing import Tuple
 from torch.ao.quantization.experimental.apot_utils import float_to_apot, apot_to_float
 
-# class to store APoT quantizer
-# implements quantize and dequantize
-# and stores all quantization parameters
+# class to store APoT quantizer and
+# implement quantize and dequantize
 class APoTQuantizer():
-    b: int
-    k: int
-    n: int
-    signed: bool
+    alpha: torch.Tensor
+    gamma: torch.Tensor
     quantization_levels: torch.Tensor
     level_indices: torch.Tensor
 
     def __init__(
         self,
-        b,
-        k,
-        max_val,
-        signed,
-            dtype=torch.quint8) -> None:
-        self.signed = signed
+        alpha: torch.Tensor,
+        gamma: torch.Tensor,
+        quantization_levels: torch.Tensor,
+            level_indices: torch.Tensor) -> None:
+        self.alpha = alpha
+        self.gamma = gamma
+        self.quantization_levels = quantization_levels
+        self.level_indices = level_indices
 
-        # check for valid inputs of b, k
-        assert(k and k != 0)
-        assert(b % k == 0)
-        self.b = b
-        self.k = k
-        self.n = b // k
-
-        # make observer, get quantizion levels and level indices
-        obs = APoTObserver(max_val=max_val, b=b, k=k)
-        obs_result = obs.calculate_qparams(signed=signed)
-        self.quantization_levels = obs_result[1]
-        self.level_indices = obs_result[2]
-
-    r""" Quantizes fp Tensor to integer APoT representatio.
-    Conversion is based on the calculated quantization levels from a specified APoT non-uniform observer.
+    r""" Quantizes fp Tensor to integer APoT representation.
+    Conversion is based on the qparams from a specified APoT non-uniform observer.
     The approach follows the method outlined in the APoT paper: https://arxiv.org/pdf/1909.13144.pdf.
     Args:
         tensor2quantize: fp Tensor
@@ -47,28 +33,41 @@ class APoTQuantizer():
     def quantize_APoT(self, tensor2quantize: Tensor):
         result = torch.tensor([])
         # map float_to_apot over tensor2quantize elements
-        result = tensor2quantize.apply_(lambda x: float_to_apot(x, self.quantization_levels, self.level_indices))
+        result_data = tensor2quantize.apply_(lambda x: float_to_apot(x, self.quantization_levels, self.level_indices))
+
+        from torch.ao.quantization.experimental.APoT_tensor import TensorAPoT
+
+        result = TensorAPoT(self, result_data)
 
         return result
 
-    r""" Dequantizes integer Tensor to floating point representation
+    r""" Dequantizes integer Tensor to floating point (fp) representation
     based on the calculated quantization levels from a specified APoT non-uniform observer.
     The approach follows the method outlined in the APoT paper: https://arxiv.org/pdf/1909.13144.pdf.
     Args:
-        self: APoTQuantizer with attr data to dequantize
+        float2apot: quantized APoT Tensor to dequantize
     Returns:
-        result: floating point representation of input Tensor
+        result: fp representation of input Tensor
     """
-    def dequantize(self, float2apot: Tensor):  # type: ignore[override]
-        float2apot = float2apot.float()
-
-        quantization_levels = self.quantization_levels
-        level_indices = self.level_indices
+    def dequantize_APoT(self, float2apot):
+        apot_tensor = float2apot.data
 
         # map apot_to_float over tensor2quantize elements
-        result = float2apot.apply_(lambda x: float(apot_to_float(x, quantization_levels, level_indices)))
+        result = apot_tensor.apply_(lambda x: float(apot_to_float(x, self.quantization_levels, self.level_indices)))
 
         return result
 
     def q_apot_alpha(self) -> float:
         raise NotImplementedError
+
+r""" Global method to create quantizer and call quantizer quantize_APoT
+"""
+def quantize_APoT(tensor2quantize: Tensor, qparams: Tuple):
+    quantizer = APoTQuantizer(alpha=qparams[0], gamma=qparams[1], quantization_levels=qparams[2], level_indices=qparams[3])
+    return quantizer.quantize_APoT(tensor2quantize)
+
+r""" Global method to create quantizer and call quantizer dequantize_APoT
+"""
+def dequantize_APoT(float2apot, qparams: Tuple):
+    quantizer = APoTQuantizer(alpha=qparams[0], gamma=qparams[1], quantization_levels=qparams[2], level_indices=qparams[3])
+    return quantizer.dequantize_APoT(float2apot)
